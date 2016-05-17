91026ffa5df3998039a4afa3efc3e3a0b0001755
Perkovec <perkovec24@gmail.com>
2016-05-17 18:52:30 +0300

    Move "translateX" method to "translate"

--- src/objects/Object.js
+++ src/objects/Object.js
@@ -1,10 +1,21 @@
 Perkogine.Object = function(properties) {
   this.visible = properties.visible || true;
   this.position = properties.position || new Perkogine.Vector2D();
+  this.rotation = properties.rotation || 0;
+  this.scale = properties.scale || 1;
 }
 
 Perkogine.Object.prototype.constructor = Perkogine.Object;
 
-Perkogine.Object.prototype.translateX = function(x) {
-  this.position.x += x;
+Perkogine.Object.prototype.translate = function(distance) {
+  this.position.x += Math.cos(Perkogine.Deg2Rad * this.rotation) * distance;
+  this.position.y += Math.sin(Perkogine.Deg2Rad * this.rotation) * distance;
+  
+  return this;
+}
+
+Perkogine.Object.prototype.rotate = function(angle) {
+  this.rotation += angle;
+  
+  return this;
 }
