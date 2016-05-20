810be14e2cf95a4f84d14c4ea779b3045367ed12
Perkovec <perkovec24@gmail.com>
2016-05-20 23:47:41 +0300

    [Circle] Update object bounds or size when position or radius changed [Rectangle] Update object bounds when position or size changed

--- src/objects/Circle.js
+++ src/objects/Circle.js
@@ -1,11 +1,49 @@
 Perkogine.Circle = function(properties) {
   Perkogine.Object.call(this, arguments);
   
-  this.radius = properties.radius || 0;
   this.color = properties.color || '#FFFFFF';
   this.borderColor = properties.borderColor || '#FFFFFF';
   this.borderWidth = properties.borderWidth || 0;
   this.texture = properties.texture || null;
+  
+  var position = this.position.clone();
+  var radius = properties.radius || 0;
+  
+  var scope = this;
+  function updateBounds() {
+    scope.bounds = {
+      left: position.x - radius,
+      right: position.x + radius,
+      top: position.y - radius,
+      bottom: position.y + radius
+    };
+  }
+  
+  Object.defineProperty(this, 'radius', {
+    get: function() { return radius; },
+    set: function(newRadius) {
+      this.width = newRadius * 2;
+      this.height = newRadius * 2;
+      radius = newRadius;
+      updateBounds();
+    }
+  });
+  
+  Object.defineProperty(this.position, 'x', {
+    get: function() { return position.x; },
+    set: function(newX) {
+      position.x = newX;
+      updateBounds()
+    }
+  });
+  
+  Object.defineProperty(this.position, 'y', {
+    get: function() { return position.y; },
+    set: function(newY) {
+      position.y = newY;
+      updateBounds()
+    }
+  });
 }
 
 Perkogine.Circle.prototype = Object.create(Perkogine.Object.prototype);
--- src/objects/Rectangle.js
+++ src/objects/Rectangle.js
@@ -1,12 +1,58 @@
 Perkogine.Rectangle = function(properties) {
   Perkogine.Object.call(this, arguments);
   
-  this.width = properties.width || 0;
-  this.height = properties.height || 0;
   this.color = properties.color || '#FFFFFF';
   this.borderColor = properties.borderColor || '#FFFFFF';
   this.borderWidth = properties.borderWidth || 0;
   this.texture = properties.texture || null;
+  
+  var width = properties.width || 0;
+  var height = properties.height || 0;
+  var position = this.position.clone();
+  
+  var scope = this;
+  function updateBounds() {
+    scope.bounds = {
+      left: position.x - width / 2.0,
+      right: position.x + width / 2.0,
+      top: position.y - height / 2.0,
+      bottom: position.y + height / 2.0
+    };
+  }
+  
+  Object.defineProperty(this, 'width', {
+    get: function() { return width; },
+    set: function(newWidth) {
+      width = newWidth;
+      updateBounds();
+    }
+  });
+  
+  Object.defineProperty(this, 'height', {
+    get: function() { return height; },
+    set: function(newHeight) {
+      height = newHeight;
+      updateBounds();
+    }
+  });
+  this.width = width;
+  this.height = height;
+  
+  Object.defineProperty(this.position, 'x', {
+    get: function() { return position.x; },
+    set: function(newX) {
+      position.x = newX;
+      updateBounds()
+    }.bind(this)
+  });
+  
+  Object.defineProperty(this.position, 'y', {
+    get: function() { return position.y; },
+    set: function(newY) {
+      position.y = newY;
+      updateBounds()
+    }.bind(this)
+  });
 }
 
 Perkogine.Rectangle.prototype = Object.create(Perkogine.Object.prototype);
