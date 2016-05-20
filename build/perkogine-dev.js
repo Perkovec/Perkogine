a20caa3f9d144d02b6f0c83bd8b9e90fbaee8957
Perkovec <perkovec24@gmail.com>
2016-05-20 23:48:47 +0300

    Update build and examples

--- build/perkogine-dev.js
+++ build/perkogine-dev.js
@@ -108,6 +108,28 @@ Perkogine.Vector2D.prototype.divideScalar = function(scalar) {
 
   return this;
 }
+
+Perkogine.Vector2D.prototype.clamp = function(min, max) {
+  this.x = Math.max(min.x, Math.min(max.x, this.x));
+  this.y = Math.max(min.y, Math.min(max.y, this.y));
+  
+  return this;
+}
+
+Perkogine.Vector2D.prototype.negate = function() {
+  this.x = -this.x;
+  this.y = -this.y;
+  
+  return this;
+}
+
+Perkogine.Vector2D.prototype.length = function() {
+  return this.x * this.x + this.y * this.y;  
+}
+
+Perkogine.Vector2D.prototype.setLength = function(length) {
+  return this.multiplyScalar(length / this.length());
+}
 Perkogine.Scene = function() {
   this.objects = [];
 }
@@ -120,6 +142,7 @@ Perkogine.Renderer = function(properties) {
   this.parent = properties.parent || document.body;
   this.width = properties.width || 500;
   this.height = properties.height || 500;
+  this.clearColor = properties.clearColor || "#FFFFFF";
   
   var domElement = document.createElement('canvas');
   domElement.width = this.width;
@@ -130,10 +153,35 @@ Perkogine.Renderer = function(properties) {
   
   this.domElement = domElement;
   this._ctx = ctx;
+  
+  this.pointerLocked = false;
 }
 
 Perkogine.Renderer.prototype.clear = function() {
-  this._ctx.clearRect(0, 0, this.width, this.height);
+  this._ctx.fillStyle = this.clearColor; 
+  this._ctx.fillRect(0, 0, this.width, this.height);
+}
+
+Perkogine.Renderer.prototype.enableLockPointer = function(onlock, onunlock) {
+  var canvas = this.domElement;
+         
+  canvas.onclick = function() {
+    canvas.requestPointerLock();
+  }
+  
+  if (document.onpointerlockchange !== undefined) {
+    document.addEventListener('pointerlockchange', pointerlockchange.bind(this), false);
+  }
+  
+  function pointerlockchange(){
+    if(document.pointerLockElement === canvas) {
+      this.pointerLocked = true;
+      onlock();
+    } else {     
+      this.pointerLocked = false;
+      onunlock();
+    }
+  }
 }
 
 Perkogine.Renderer.prototype.Render = function(scene) {
@@ -163,6 +211,8 @@ Perkogine.Renderer.prototype.Render = function(scene) {
       DrawEllipse(object);
     } else if (object instanceof Perkogine.PathShape) {
       DrawPathShape(object);
+    } else if (object instanceof Perkogine.Text) {
+      DrawText(object);
     }
   }
   
@@ -225,12 +275,24 @@ Perkogine.Renderer.prototype.Render = function(scene) {
     ctx.strokeWidth = object.strokeWidth;
     ctx.stroke();
   }
+  
+  function DrawText(object){
+    ctx.beginPath();
+    ctx.font = object.fontSize + "px " + object.font;
+    ctx.fillStyle = (object.texture !== null) ? ctx.createPattern(object.texture, 'repeat') : object.color;
+    ctx.strokeStyle = object.borderColor;
+    ctx.strokeWidth = object.strokeWidth;
+    ctx.fillText(object.text, object.position.x, object.position.y);
+  }
 }
 Perkogine.Object = function(properties) {
   this.visible = properties.visible || true;
   this.position = properties.position || new Perkogine.Vector2D();
   this.rotation = properties.rotation || 0;
   this.scale = properties.scale || 1;
+  this.width = properties.width || 0;
+  this.height = properties.height || 0;
+  this.bounds = properties.bounds || {};
 }
 
 Perkogine.Object.prototype.constructor = Perkogine.Object;
@@ -289,11 +351,49 @@ Perkogine.PathShape.prototype.clone = function() {
 Perkogine.Circle = function(properties) {
   Perkogine.Object.call(this, arguments);
   
-  this.radius = properties.radius || 0;
   this.color = properties.color || '#FFFFFF';
   this.borderColor = properties.borderColor || '#FFFFFF';
   this.borderWidth = properties.borderWidth || 0;
   this.texture = properties.texture || null;
+  
+  var position = this.position.clone();
+  var radius = properties.radius || 0;
+  
+  var scope = this;
+  function updateBounds() {
+    scope.bounds = {
+      left: position.x - radius,
+      right: position.x + radius,
+      top: position.y - radius,
+      bottom: position.y + radius
+    };
+  }
+  
+  Object.defineProperty(this, 'radius', {
+    get: function() { return radius; },
+    set: function(newRadius) {
+      this.width = newRadius * 2;
+      this.height = newRadius * 2;
+      radius = newRadius;
+      updateBounds();
+    }
+  });
+  
+  Object.defineProperty(this.position, 'x', {
+    get: function() { return position.x; },
+    set: function(newX) {
+      position.x = newX;
+      updateBounds()
+    }
+  });
+  
+  Object.defineProperty(this.position, 'y', {
+    get: function() { return position.y; },
+    set: function(newY) {
+      position.y = newY;
+      updateBounds()
+    }
+  });
 }
 
 Perkogine.Circle.prototype = Object.create(Perkogine.Object.prototype);
@@ -305,12 +405,58 @@ Perkogine.Circle.prototype.clone = function() {
 Perkogine.Rectangle = function(properties) {
   Perkogine.Object.call(this, arguments);
   
-  this.width = properties.width || 0;
-  this.height = properties.height || 0;
   this.color = properties.color || '#FFFFFF';
   this.borderColor = properties.borderColor || '#FFFFFF';
   this.borderWidth = properties.borderWidth || 0;
   this.texture = properties.texture || null;
+  
+  var width = properties.width || 0;
+  var height = properties.height || 0;
+  var position = this.position.clone();
+  
+  var scope = this;
+  function updateBounds() {
+    scope.bounds = {
+      left: position.x - width / 2.0,
+      right: position.x + width / 2.0,
+      top: position.y - height / 2.0,
+      bottom: position.y + height / 2.0
+    };
+  }
+  
+  Object.defineProperty(this, 'width', {
+    get: function() { return width; },
+    set: function(newWidth) {
+      width = newWidth;
+      updateBounds();
+    }
+  });
+  
+  Object.defineProperty(this, 'height', {
+    get: function() { return height; },
+    set: function(newHeight) {
+      height = newHeight;
+      updateBounds();
+    }
+  });
+  this.width = width;
+  this.height = height;
+  
+  Object.defineProperty(this.position, 'x', {
+    get: function() { return position.x; },
+    set: function(newX) {
+      position.x = newX;
+      updateBounds()
+    }.bind(this)
+  });
+  
+  Object.defineProperty(this.position, 'y', {
+    get: function() { return position.y; },
+    set: function(newY) {
+      position.y = newY;
+      updateBounds()
+    }.bind(this)
+  });
 }
 
 Perkogine.Rectangle.prototype = Object.create(Perkogine.Object.prototype);
@@ -334,6 +480,24 @@ Perkogine.Ellipse.prototype = Object.create(Perkogine.Object.prototype);
 Perkogine.Ellipse.prototype.constructor = Perkogine.Ellipse;
 
 Perkogine.Ellipse.prototype.clone = function() {
+  return new this.constructor(this).copy(this);
+}
+Perkogine.Text = function(properties) {
+  Perkogine.Object.call(this, arguments);
+  
+  this.color = properties.color || '#FFFFFF';
+  this.borderColor = properties.borderColor || '#FFFFFF';
+  this.borderWidth = properties.borderWidth || 0;
+  this.texture = properties.texture || null;
+  this.font = properties.font || "Arial";
+  this.fontSize = properties.fontSize || 16;
+  this.text = properties.text || "";
+}
+
+Perkogine.Text.prototype = Object.create(Perkogine.Object.prototype);
+Perkogine.Text.prototype.constructor = Perkogine.Ellipse;
+
+Perkogine.Text.prototype.clone = function() {
   return new this.constructor(this).copy(this);
 }
 Perkogine.CountManager = function(count, callback) {
@@ -371,3 +535,80 @@ Perkogine.AssetsManager.prototype.loadImages = function(links, callback) {
     }).bind(this)(loadManager, links[i]);
   }
 }
+Perkogine.Keyboard = function(target) {
+  this.keys = {};
+  
+  target.addEventListener('keydown', keydown.bind(this), false);
+  target.addEventListener('keyup', keyup.bind(this), false);
+  
+  function keydown(event) {
+    this.keys[event.keyCode || event.which] = true;
+		this.keys["shift"] = event.shiftKey;
+		this.keys["ctrl"] = event.ctrlKey;
+		this.keys["alt"] = event.altKey;
+  }
+  
+  function keyup(event) {
+    this.keys[event.keyCode || event.which] = false;
+		this.keys["shift"] = event.shiftKey;
+		this.keys["ctrl"] = event.ctrlKey;
+		this.keys["alt"] = event.altKey;
+  }
+}
+
+Perkogine.Keys = {};
+
+Object.defineProperties( Perkogine.Keys, {
+  'Tab': { value: 9 },
+  'Esc': { value: 27 },
+  'Ins': { value: 45 },
+  'Del': { value: 46 },
+  'Space': { value: 32 },
+  'Caps_Lock': { value: 20 },
+  'Shift': { value: 'shift' },
+  'Ctrl': { value: 'ctrl' },
+  'Alt': { value: 'alt' },
+  
+  'UP': { value: 38 },
+  'DOWN': { value: 40 },
+  'LEFT': { value: 37 },
+  'RIGHT': { value: 39 },
+  
+  'Num_0': { value: 48 },
+  'Num_1': { value: 49 },
+  'Num_2': { value: 50 },
+  'Num_3': { value: 51 },
+  'Num_4': { value: 52 },
+  'Num_5': { value: 53 },
+  'Num_6': { value: 54 },
+  'Num_7': { value: 55 },
+  'Num_8': { value: 56 },
+  'Num_9': { value: 57 },
+  
+  'A': { value: 65 },
+  'B': { value: 66 },
+  'C': { value: 67 },
+  'D': { value: 68 },
+  'E': { value: 69 },
+  'F': { value: 70 },
+  'G': { value: 71 },
+  'H': { value: 72 },
+  'I': { value: 73 },
+  'J': { value: 74 },
+  'K': { value: 75 },
+  'L': { value: 76 },
+  'M': { value: 77 },
+  'N': { value: 78 },
+  'O': { value: 79 },
+  'P': { value: 80 },
+  'Q': { value: 81 },
+  'R': { value: 82 },
+  'S': { value: 83 },
+  'T': { value: 84 },
+  'U': { value: 85 },
+  'V': { value: 86 },
+  'W': { value: 87 },
+  'X': { value: 88 },
+  'Y': { value: 89 },
+  'Z': { value: 90 }
+});
