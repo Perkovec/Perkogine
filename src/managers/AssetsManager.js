a2294d12e4e221297540af2baf2900e7365ff68c
Perkovec <perkovec24@gmail.com>
2016-05-30 22:45:03 +0300

    [AssetsManager] Use XHR

--- src/managers/AssetsManager.js
+++ src/managers/AssetsManager.js
@@ -6,18 +6,36 @@ Perkogine.AssetsManager.prototype.loadImages = function(links, callback) {
   if (!links.length){
     return;
   }
+  var scope = this;
   
   var loadManager = new Perkogine.CountManager(links.length, callback);
   
+  function Base64FromURL(linkData, callback){
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'blob';
+    xhr.onload = function() {
+        var reader  = new FileReader();
+        reader.onloadend = function () {
+            callback(reader.result, linkData);
+        }
+        reader.readAsDataURL(xhr.response);
+    };
+    xhr.open('GET', linkData.source);
+    xhr.send();
+  }
+
   for (var i = 0; i < links.length; ++i){
-    (function(manager, link) {
-      var scope = this;
-      var image = new Image();
-      image.onload = function() {
-        scope.assets[link.name] = image;
-        loadManager.tick();
+    Base64FromURL(
+      links[i],
+      function(data, link) {
+        var image = new Image();
+        image.onload = function() {
+          scope.assets[link.name] = image;
+          loadManager.tick();
+        }
+        image.crossOrigin = "Anonymous";
+        image.src = link.source;
       }
-      image.src = link.source;
-    }).bind(this)(loadManager, links[i]);
+    );
   }
 }
